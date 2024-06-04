a1117ec4461f660759ef8e9b521fdf4ecf914615
Jamaninja <mattcarlyon99@gmail.com>
2024-06-04 16:15:12 +0100

    Fixed datatype saving issue, implemented plotting functions
    Fixed issue where orekit datatypes were not being saved alongside
    data, resulting in code failing after data was read. Now, no
    orekit-specific data is saved, only data of generic types, which is
    then processed for use in orekit.
    
    Seperated OrbitographyFunctions class into SatelliteFunctions and
    PlottingFunctions, moved code required for visualing orbits into the
    latter class.

--- Orbit_Test.py
+++ Orbit_Test.py
@@ -1,197 +1,34 @@
-sat_data = {
-    'ENVISAT': {
-        'norad_id': 27386,  # For Space-Track TLE queries
-        'cospar_id': '0200901',  # For laser ranging data queries
-        'sic_id': '6179',  # For writing in CPF files
-        'mass': 8000.0, # kg; TODO: compute proper value
-        'cross_section': 100.0, # m2; TODO: compute proper value
-        'cd': 2.0, # TODO: compute proper value
-        'cr': 1.0  # TODO: compute proper value
-    },
-    'LAGEOS2': {
-        'norad_id': 22195,
-        'cospar_id': '9207002',
-        'sic_id': '5986',
-        'mass': 405.0, # kg
-        'cross_section': 0.2827, # m2
-        'cd': 2.0, # TODO: compute proper value
-        'cr': 1.0  # TODO: compute proper value
-    },
-    'TECHNOSAT': {
-        'norad_id': 42829,
-        'cospar_id': '1704205',
-        'sic_id': '6203',
-        'mass': 20.0, # kg
-        'cross_section': 0.10, # m2,
-        'cd': 2.0, # TODO: compute proper value
-        'cr': 1.0  # TODO: compute proper value
-    },
-    'SNET1': {
-        'norad_id': 43189,
-        'cospar_id': '1801410',
-        'sic_id': '6204',
-        'mass': 8.0, # kg
-        'cross_section': 0.07, # m2
-        'cd': 2.0, # TODO: compute proper value
-        'cr': 1.0  # TODO: compute proper value
-    },
-    'ISS (ZARYA)': {
-        'norad_id': 25544,
-        'cospar_id': '1998-067A',
-        'sic_id': '',
-        'mass': 450_000.0, # kg
-        'cross_section': 2_481.0, # m2
-        'cd': 2.0, # TODO: compute proper value
-        'cr': 1.0  # TODO: compute proper value
-    }
-}
-
-##############################################################################################################
-
 import orekit
 vm = orekit.initVM()
 from orekit.pyhelpers import setup_orekit_curdir, absolutedate_to_datetime
 setup_orekit_curdir()
 
-from org.orekit.data import DataProvidersManager, ZipJarCrawler # type: ignore
-from org.orekit.frames import FramesFactory, TopocentricFrame # type: ignore
-from org.orekit.bodies import OneAxisEllipsoid, GeodeticPoint # type: ignore
-from org.orekit.time import TimeScalesFactory, AbsoluteDate, DateTimeComponents # type: ignore
-from org.orekit.utils import IERSConventions, Constants # type: ignore
-from org.orekit.propagation.analytical.tle import TLE, TLEPropagator # type: ignore
-from org.orekit.propagation.numerical import NumericalPropagator # type: ignore
-from org.orekit.propagation import SpacecraftState  # type: ignore
-from org.orekit.orbits import OrbitType, PositionAngleType, CartesianOrbit # type: ignore
-from org.hipparchus.ode.nonstiff import DormandPrince853Integrator # type: ignore
-from org.orekit.forces.gravity.potential import GravityFieldFactory # type: ignore
-from org.orekit.forces.gravity import HolmesFeatherstoneAttractionModel # type: ignore
-from orekit import JArray_double
-
-from java.util import Arrays # type: ignore
-from java.io import File # type: ignore
-
 import pandas as pd
 import numpy as np
-from spacetrack import SpaceTrackClient
-from datetime import datetime, timedelta, UTC
-from PIL import Image
-import plotly.graph_objs as go
+from Orbitography_Functions import SatelliteFunctions, PlotFunctions
 
-from dotenv import load_dotenv
-import os
-load_dotenv()
+sat_data = pd.read_pickle("Satellite_Data.pkl")
+sf = SatelliteFunctions(sat_data=sat_data)
 
-from Orbitography_Functions import OrbitographyFunctions
-
-##############################################################################################################
-
-# Define Earth and inertial reference frames
-eme2000     = FramesFactory.getEME2000()
-itrf        = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
-
-id_st       = os.getenv("id_st")
-pass_st     = os.getenv("pass_st")
-st          = SpaceTrackClient(identity=id_st, password=pass_st)
-
-now         = datetime.now(UTC)
-if now.minute != 59:
-    time_now = AbsoluteDate(now.year, now.month, now.day, now.hour, now.minute + 1, 0.0, TimeScalesFactory.getUTC())
-elif now.hour != 23:
-    time_now = AbsoluteDate(now.year, now.month, now.day, now.hour + 1, 0, 0.0, TimeScalesFactory.getUTC())
-else:
-    time_now = AbsoluteDate(now.year, now.month, now.day + 1, 0, 0, 0.0, TimeScalesFactory.getUTC())
+prop_data = {}
 
 for sat in sat_data:
-    tle = st.tle(norad_cat_id=sat_data[sat]['norad_id'], epoch='<{}'.format(time_now), orderby='epoch desc', limit=1, format='tle').split('\n')
-    sat_data[sat]['TLE'] = TLE(tle[0],tle[1])
+    prop_data.update(     {sat          : {}})
+    pvs = sf.propagateNumerical(sat, prop_dur=.25, prop_res=60.0)
 
-of = OrbitographyFunctions(sat_data=sat_data)
-
-prop_data, trace_orbit = {},[]
-
-for sat in sat_data:
-    prop_data.update({sat : {}})
-    # Extracts position-velocity data and creates database of calculated values
-    prop_data[sat].update({'pv'         : of.propagateNumerical(sat, prop_dur=.25, prop_res=60.0)})
-    prop_data[sat].update({'position'   : [pv.getPosition() for pv in prop_data[sat]['pv']]})
-    prop_data[sat].update({'x'          : [pos.x for pos in prop_data[sat]['position']],
-                           'y'          : [pos.y for pos in prop_data[sat]['position']],
-                           'z'          : [pos.z for pos in prop_data[sat]['position']]})
+    #prop_data[sat].update({'position'   : [pv.getPosition() for pv in prop_data[sat]['pv']]})
+    prop_data[sat].update({'x'          : [pv.getPosition().x for pv in pvs],
+                           'y'          : [pv.getPosition().y for pv in pvs],
+                           'z'          : [pv.getPosition().z for pv in pvs]})   
     prop_data[sat].update({'radius'     : [np.sqrt(x**2 + y**2 + z**2) for x, y, z in zip(prop_data[sat]['x'], prop_data[sat]['y'], prop_data[sat]['z'])],
-                           'datetime'   : [absolutedate_to_datetime(pv.getDate()) for pv in prop_data[sat]['pv']],
-                           'groundpoint': [of.earth(eme2000).transform(pv.position, eme2000, pv.date) for pv in prop_data[sat]['pv']]})
-    prop_data[sat].update({'latitude'   : np.degrees([gp.latitude for gp in prop_data[sat]['groundpoint']]),
-                           'longitude'  : np.degrees([gp.longitude for gp in prop_data[sat]['groundpoint']])})
+                           'datetime'   : [absolutedate_to_datetime(pv.getDate()) for pv in pvs]})
+    groundpoints = [sf.earth(sf.eme2000).transform(pv.position, sf.eme2000, pv.date) for pv in pvs]
+    prop_data[sat].update({'latitude'   : np.degrees([gp.latitude for gp in groundpoints]),
+                           'longitude'  : np.degrees([gp.longitude for gp in groundpoints]),
+                           'epoch'      : absolutedate_to_datetime(sf.toTLE(sat_data.get(sat)['TLE']).getDate())})
 
 prop_dataframe = pd.DataFrame(prop_data)
-prop_dataframe.to_csv('Propagation_Data.csv', index=False)
-    
-for sat in sat_data:
+prop_dataframe.to_pickle('Propagation_Data.pkl')
 
-    # Defines label text when hovering over orbits and creates orbit traces
-        # Satellite name
-        # Latitude & longitude
-        # Date & time (UTC)
-        # Time since epoch
-
-    text = []
-    name = sat
-    lat_lambda = lambda x: 'N' if x >= 0 else 'S'
-    lon_lambda = lambda x: 'E' if x >= 0 else 'W'
-
-    for lat, lon, r, dt in zip(prop_data[sat]['latitude'], prop_data[sat]['longitude'], prop_data[sat]['radius'], prop_data[sat]['datetime']):    
-        text.append('''{}<br>{}
-<br>{:02}° {:02}\' {:02.4f}\" {}, {:02}° {:02}\' {:02.4f}\" {}
-<br>Radius (Alt): {:.2f} km ({:.2f} km)<br>{}
-<br>{} {}<br>epoch+{}'''.format(name, '-'*56,
-                                int(abs(lat)), int(abs(lat)%1*60), abs(lat)%1*3600%60, lat_lambda(lat), int(abs(lon)), int(abs(lon)%1*60), abs(lon)%1*3600%60, lon_lambda(lon), 
-                                r/1000, (r - Constants.WGS84_EARTH_EQUATORIAL_RADIUS)/1000,'-'*56,
-                                dt.strftime('%Y-%m-%d %H:%M:%S'), 'UTC', of.getEpochDelta(sat)
-                                )
-                    )
-
-    trace_orbit.append(go.Scatter3d(x=[x for x in prop_data[sat]['x']],
-                                    y=[y for y in prop_data[sat]['y']],
-                                    z=[z for z in prop_data[sat]['z']],
-                                    marker=dict(size=0.5),
-                                    line=dict(color='white',width=1),
-                                    hoverinfo='text',
-                                    hovertemplate='%{text}<extra></extra>',
-                                    text = text
-                                    )
-                       )
-
-'''
-proximity_bound = 10e3 # meters
-# Detecting collisions
-    # First, determine which satellites are within a 10km box
-while True:
-    a,b = 0,1
-    sat_a, sat_b = sat_data[a], sat_data[b]
-    if abs(prop_data[sat_a]['x'] - prop_data[sat_b]['x']) > proximity_bound:
-        break
-    elif abs(prop_data[sat_a]['y'] - prop_data[sat_b]['y']) > proximity_bound:
-        break
-    elif abs(prop_data[sat_a]['z'] - prop_data[sat_b]['z']) > proximity_bound:
-        break
-    else:
-        # Calculate distance between sat_a and sat_b
-        print()
-'''
-
-trace_earth = of.plotEarth()
-trace_background = of.plotBackground()
-
-layout = go.Layout(paper_bgcolor='black',
-                   scene = dict(xaxis = dict(visible=False),
-                                yaxis = dict(visible=False),
-                                zaxis = dict(visible=False),
-                                hovermode = 'closest',
-                                ),
-                   showlegend = False
-                   )
-
-fig = go.Figure(data=[trace_earth, trace_background] + trace_orbit, 
-                layout=layout,
-                )
-fig.show()
+pf = PlotFunctions(prop_data_file='Propagation_Data.pkl')
+pf.plotOrbits()
--- Orbitography_Functions.py
+++ Orbitography_Functions.py
@@ -1,17 +1,17 @@
 import orekit
 vm = orekit.initVM()
-from orekit.pyhelpers import setup_orekit_curdir, absolutedate_to_datetime
+from orekit.pyhelpers import setup_orekit_curdir, datetime_to_absolutedate
 setup_orekit_curdir()
 
-from org.orekit.data import DataProvidersManager, ZipJarCrawler # type: ignore
-from org.orekit.frames import FramesFactory, TopocentricFrame # type: ignore
-from org.orekit.bodies import OneAxisEllipsoid, GeodeticPoint # type: ignore
-from org.orekit.time import TimeScalesFactory, AbsoluteDate, DateTimeComponents # type: ignore
+
+from org.orekit.frames import FramesFactory # type: ignore
+from org.orekit.bodies import OneAxisEllipsoid # type: ignore
+from org.orekit.time import TimeScalesFactory, AbsoluteDate # type: ignore
 from org.orekit.utils import IERSConventions, Constants # type: ignore
 from org.orekit.propagation.analytical.tle import TLE, TLEPropagator # type: ignore
 from org.orekit.propagation.numerical import NumericalPropagator # type: ignore
 from org.orekit.propagation import SpacecraftState  # type: ignore
-from org.orekit.orbits import OrbitType, PositionAngleType, CartesianOrbit # type: ignore
+from org.orekit.orbits import OrbitType, CartesianOrbit # type: ignore
 from org.hipparchus.ode.nonstiff import DormandPrince853Integrator # type: ignore
 from org.orekit.forces.gravity.potential import GravityFieldFactory # type: ignore
 from org.orekit.forces.gravity import HolmesFeatherstoneAttractionModel # type: ignore
@@ -20,24 +20,23 @@ from orekit import JArray_double
 from java.util import Arrays # type: ignore
 from java.io import File # type: ignore
 
+import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta, UTC
 import plotly.graph_objs as go
 from PIL import Image
 
-class OrbitographyFunctions:
+now = datetime.now(UTC)
+if now.minute != 59:
+    time_now = AbsoluteDate(now.year, now.month, now.day, now.hour, now.minute + 1, 0.0, TimeScalesFactory.getUTC())
+elif now.hour != 23:
+    time_now = AbsoluteDate(now.year, now.month, now.day, now.hour + 1, 0, 0.0, TimeScalesFactory.getUTC())
+else:
+    time_now = AbsoluteDate(now.year, now.month, now.day + 1, 0, 0, 0.0, TimeScalesFactory.getUTC())
 
-    def __init__(self, sat_data):
-        self.sat_data   = sat_data
-        self.eme2000    = FramesFactory.getEME2000()
-        self.itrf       = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
-        now             = datetime.now(UTC)
-        if now.minute != 59:
-            self.time_now = AbsoluteDate(now.year, now.month, now.day, now.hour, now.minute + 1, 0.0, TimeScalesFactory.getUTC())
-        elif now.hour != 23:
-            self.time_now = AbsoluteDate(now.year, now.month, now.day, now.hour + 1, 0, 0.0, TimeScalesFactory.getUTC())
-        else:
-            self.time_now = AbsoluteDate(now.year, now.month, now.day + 1, 0, 0, 0.0, TimeScalesFactory.getUTC())
+class PlotFunctions:
+    def __init__(self, prop_data_file):
+        self.prop_data = pd.read_pickle(prop_data_file)
 
     def createSpheroid(self, texture, equatorial_radius, flattening=0):
         """
@@ -62,9 +61,9 @@ class OrbitographyFunctions:
         phi   = np.linspace(0,np.pi,N_lon) 
 
         axes_radii = [equatorial_radius * np.outer(np.cos(theta),np.sin(phi)),
-                    equatorial_radius * np.outer(np.sin(theta),np.sin(phi)),
-                    equatorial_radius * (1 - flattening) * np.outer(np.ones(N_lat),np.cos(phi))
-                    ]
+                      equatorial_radius * np.outer(np.sin(theta),np.sin(phi)),
+                      equatorial_radius * (1 - flattening) * np.outer(np.ones(N_lat),np.cos(phi))]
+        
         return axes_radii
 
     def plotEarth(self):
@@ -88,11 +87,11 @@ class OrbitographyFunctions:
 
         axes = self.createSpheroid(earth_texture, Constants.IERS2010_EARTH_EQUATORIAL_RADIUS, Constants.IERS2010_EARTH_FLATTENING)
         surf = go.Surface(x=axes[0], y=axes[1], z=axes[2],
-                        surfacecolor = earth_texture,
-                        colorscale   = colorscale,
-                        showscale    = False,
-                        hoverinfo    = 'skip',
-                        )    
+                          surfacecolor = earth_texture,
+                          colorscale   = colorscale,
+                          showscale    = False,
+                          hoverinfo    = 'skip',
+                          )    
         return surf
 
     def plotBackground(self):
@@ -109,7 +108,7 @@ class OrbitographyFunctions:
                         hoverinfo  = 'skip',
                         )    
         return surf
-
+    
     def getEpochDelta(self, sat):
         """
         Calculates time between the epoch of the current satellite and time_now
@@ -120,13 +119,97 @@ class OrbitographyFunctions:
             TimeDelta of time since epoch
         """
 
-        epoch = self.sat_data.get(sat)['TLE'].getDate()
-        return timedelta(seconds = self.time_now.durationFrom(epoch))
+        epoch = self.prop_data.get(sat)['epoch']
+        return timedelta(seconds = time_now.durationFrom(datetime_to_absolutedate(epoch)))
+
+    def plotOrbits(self):
+
+        trace_orbit = []
+
+        for sat in self.prop_data:
+
+        # Defines label text when hovering over orbits and creates orbit traces
+            # Satellite name
+            # Latitude & longitude
+            # Date & time (UTC)
+            # Time since epoch
+
+            text = []
+            name = sat
+            lat_lambda = lambda x: 'N' if x >= 0 else 'S'
+            lon_lambda = lambda x: 'E' if x >= 0 else 'W'
+
+            for lat, lon, r, dt in zip(self.prop_data[sat]['latitude'], self.prop_data[sat]['longitude'], self.prop_data[sat]['radius'], self.prop_data[sat]['datetime']):    
+                text.append('''{}<br>{}
+<br>{:02}° {:02}\' {:02.4f}\" {}, {:02}° {:02}\' {:02.4f}\" {}
+<br>Radius (Alt): {:.2f} km ({:.2f} km)<br>{}
+<br>{} {}<br>epoch+{}'''.format(name, '-'*56,
+                                int(abs(lat)), int(abs(lat)%1*60), abs(lat)%1*3600%60, lat_lambda(lat), int(abs(lon)), int(abs(lon)%1*60), abs(lon)%1*3600%60, lon_lambda(lon), 
+                                r/1000, (r - Constants.WGS84_EARTH_EQUATORIAL_RADIUS)/1000,'-'*56,
+                                dt.strftime('%Y-%m-%d %H:%M:%S'), 'UTC', self.getEpochDelta(sat)
+                                )
+                    )
+
+            trace_orbit.append(go.Scatter3d(x=[x for x in self.prop_data[sat]['x']],
+                                            y=[y for y in self.prop_data[sat]['y']],
+                                            z=[z for z in self.prop_data[sat]['z']],
+                                            marker=dict(size=0.5),
+                                            line=dict(color='white',width=1),
+                                            hoverinfo='text',
+                                            hovertemplate='%{text}<extra></extra>',
+                                            text = text
+                                            )
+                       )
+
+        '''
+        proximity_bound = 10e3 # meters
+        # Detecting collisions
+            # First, determine which satellites are within a 10km box
+        while True:
+            a,b = 0,1
+            sat_a, sat_b = sat_data[a], sat_data[b]
+            if abs(prop_data[sat_a]['x'] - prop_data[sat_b]['x']) > proximity_bound:
+                break
+            elif abs(prop_data[sat_a]['y'] - prop_data[sat_b]['y']) > proximity_bound:
+                break
+            elif abs(prop_data[sat_a]['z'] - prop_data[sat_b]['z']) > proximity_bound:
+                break
+            else:
+                # Calculate distance between sat_a and sat_b
+                print()
+        '''
+
+        trace_earth = self.plotEarth()
+        trace_background = self.plotBackground()
+
+        layout = go.Layout(paper_bgcolor='black',
+                           scene = dict(xaxis = dict(visible=False),
+                                        yaxis = dict(visible=False),
+                                        zaxis = dict(visible=False),
+                                        hovermode = 'closest',
+                                        ),
+                           showlegend = False
+                           )
+
+        fig = go.Figure(data=[trace_earth, trace_background] + trace_orbit, 
+                        layout=layout,
+                        )
+        fig.show()
+
+class SatelliteFunctions:
+
+    def __init__(self, sat_data):
+        self.sat_data   = sat_data
+        self.eme2000    = FramesFactory.getEME2000()
+        self.itrf       = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
 
     def earth(self, frame):
         return OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS, 
                                 Constants.WGS84_EARTH_FLATTENING, 
                                 frame)
+    
+    def toTLE(self, sat_tle_tuple):
+        return TLE(sat_tle_tuple[0], sat_tle_tuple[1])
 
     def initialOrbitTLE(self, sat_tle):
         """
@@ -164,8 +247,8 @@ class OrbitographyFunctions:
             Array of position and velocity coordinates
         """
             
-        extrap_date = self.time_now
-        propagator = TLEPropagator.selectExtrapolator(self.sat_data.get(sat)['TLE'])
+        extrap_date = time_now
+        propagator = TLEPropagator.selectExtrapolator(self.toTLE(self.sat_data[sat]['TLE']))
         pvs = []
         final_date = extrap_date.shiftedBy(60 * 60 * 24 * prop_dur) #seconds
 
@@ -193,8 +276,8 @@ class OrbitographyFunctions:
             Array of position and velocity coordinates
         """
 
-        initial_orbit = self.initialOrbitTLE(self.sat_data.get(sat)['TLE']) # Defines Cartesian orbit using latest TLE data
-        sat_mass = 100.0 # kg
+        initial_orbit = self.initialOrbitTLE(self.toTLE(self.sat_data[sat]['TLE'])) # Defines Cartesian orbit using latest TLE data
+        sat_mass = self.sat_data[sat]['mass']
         initial_state = SpacecraftState(initial_orbit, sat_mass)
 
         min_step = 0.001
@@ -216,7 +299,7 @@ class OrbitographyFunctions:
         gravityProvider = GravityFieldFactory.getNormalizedProvider(8, 8)
         propagator_num.addForceModel(HolmesFeatherstoneAttractionModel(self.earth(self.itrf).getBodyFrame(), gravityProvider))
 
-        start_date = self.time_now
+        start_date = time_now
 
         t = [start_date.shiftedBy(float(dt)) for dt in np.arange(0, prop_dur * 86400, prop_res)]
         pvs = [propagator_num.propagate(tt).getPVCoordinates() for tt in t]
