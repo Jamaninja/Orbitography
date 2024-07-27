6a94330ca96d350121466041b734898a5206778b
Jamaninja <mattcarlyon99@gmail.com>
2024-07-27 14:57:59 +0100

    Updated propagation function parameters
    Removed duration from parameters list, added kwarg support for either
    duration or both start and end date.
    Updated Orbit_Test.py to function with new changes to propagation
    functions

--- Orbit_Test.py
+++ Orbit_Test.py
@@ -13,22 +13,19 @@ sf = SatelliteFunctions(sat_data=sat_data)
 prop_data = {}
 
 for sat in sat_data:
+    pvs                                 = sf.propagateNumerical(sat=sat, resolution=60.0, duration=.25)
+    groundpoints                        = [sf.earth(sf.eme2000).transform(pv.position, sf.eme2000, pv.date) for pv in pvs]
     prop_data.update(     {sat          : {}})
-    pvs = sf.propagateNumerical(sat, prop_dur=.25, prop_res=60.0)
-
-    #prop_data[sat].update({'position'   : [pv.getPosition() for pv in prop_data[sat]['pv']]})
     prop_data[sat].update({'x'          : [pv.getPosition().x for pv in pvs],
                            'y'          : [pv.getPosition().y for pv in pvs],
                            'z'          : [pv.getPosition().z for pv in pvs]})   
     prop_data[sat].update({'radius'     : [np.sqrt(x**2 + y**2 + z**2) for x, y, z in zip(prop_data[sat]['x'], prop_data[sat]['y'], prop_data[sat]['z'])],
-                           'datetime'   : [absolutedate_to_datetime(pv.getDate()) for pv in pvs]})
-    groundpoints = [sf.earth(sf.eme2000).transform(pv.position, sf.eme2000, pv.date) for pv in pvs]
-    prop_data[sat].update({'latitude'   : np.degrees([gp.latitude for gp in groundpoints]),
+                           'datetime'   : [absolutedate_to_datetime(pv.getDate()) for pv in pvs],
+                           'latitude'   : np.degrees([gp.latitude for gp in groundpoints]),
                            'longitude'  : np.degrees([gp.longitude for gp in groundpoints]),
                            'epoch'      : absolutedate_to_datetime(sf.toTLE(sat_data.get(sat)['TLE']).getDate())})
 
-prop_dataframe = pd.DataFrame(prop_data)
-prop_dataframe.to_pickle('Propagation_Data.pkl')
+pd.DataFrame(prop_data).to_pickle('Propagation_Data.pkl')
 
 pf = PlotFunctions(prop_data_file='Propagation_Data.pkl')
 pf.plotOrbits()
