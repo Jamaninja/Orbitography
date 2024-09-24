6ecb4fc516bf66b2da1dec37490fc7e71ce550d7
Jamaninja <mattcarlyon99@gmail.com>
2024-09-24 13:52:06 +0100

    Minor changes to slightly speed up program + Changed df.loc[:, column] to df[column], saves ~1s per calculation + Updated pickle to json

--- Propagate_Orbits.py
+++ Propagate_Orbits.py
@@ -9,42 +9,44 @@ from Orbitography_Functions import SatelliteFunctions, PlotFunctions
 import json
 
 def importDatabase(database_path):
-    sat_data        = pd.read_pickle(database_path).filter(['NORAD_CAT_ID', 'OBJECT_NAME', 'OBJECT_TYPE', 'TLE_LINE1', 'TLE_LINE2']).set_index('NORAD_CAT_ID')
-    sat_data['TLE'] = sat_data[['TLE_LINE1', 'TLE_LINE2']].apply(lambda x: (x.iloc[0][0], x.iloc[1][0]), axis=1) # Combines TLE_LINE1 and TLE_LINE2 into a tuple in a new column TLE 
-    sat_data        = sat_data.drop(['TLE_LINE1', 'TLE_LINE2'], axis=1)
+    sat_data        = pd.read_json(database_path).filter(['OBJECT_NAME', 'OBJECT_TYPE', 'TLE_LINE1', 'TLE_LINE2'])
+    sat_data['TLE'] = sat_data[['TLE_LINE1', 'TLE_LINE2']].apply(lambda x: (x.iloc[0][0], x.iloc[1][0]), axis=1)    # Combines most recent TLE values from TLE_LINE1 and TLE_LINE2 
+    sat_data        = sat_data.drop(['TLE_LINE1', 'TLE_LINE2'], axis=1)                                             # into a tuple in a new column TLE
 
     return sat_data
 
 def propagateOrbits(sat_data):
-    sf                              = SatelliteFunctions(sat_data=sat_data)
-    prop_data                       = pd.DataFrame(columns=['object_name', 'pvs', 'pos', 'gps', 'x', 'y', 'z', 'radius', 'latitude', 'longitude', 'epoch', 'object_type'], index = sat_data.index)
-    prop_data.loc[:, 'object_name'] = sat_data.loc[:, 'OBJECT_NAME']
-    prop_data.loc[:, 'object_type'] = sat_data.loc[:, 'OBJECT_TYPE']
+    sf                      = SatelliteFunctions(sat_data=sat_data)
+    prop_data               = pd.DataFrame(columns=['object_name',  'object_type', 'pvs', 'pos', 'gps', 'x', 'y', 'z', 'radius', 'latitude', 'longitude', 'epoch'], index=sat_data.index)
+    prop_data[['object_name', 'object_type']] = sat_data[['OBJECT_NAME', 'OBJECT_TYPE']]
 
     print('Propagating orbits...')
-    prop_data.loc[:, 'pvs']         = [sf.propagateTLE(sat=sat, resolution=300., duration=.25) for sat in sat_data.index]
+    prop_data['pvs']        = [sf.propagateTLE(sat=sat, resolution=300., duration=.25) for sat in sat_data.index]
     print('Extracting epochs...')
-    prop_data.loc[:, 'epoch']       = [absolutedate_to_datetime(sf.toTLE(sat_data.loc[sat, 'TLE']).getDate()) for sat in sat_data.index]
+    prop_data.update(sat_data['TLE'].apply(lambda x: sf.toTLE(x).getDate()))
 
     print('Calculating positions...')
-    prop_data.loc[:, 'pos']         = prop_data.loc[:, 'pvs'].apply(lambda pvs: list(map(lambda pv: pv.getPosition(), pvs)))
+    prop_data['pos']        = prop_data['pvs'].apply(lambda pvs: list(map(lambda pv: pv.getPosition(), pvs)))
     print('Calculating x coordinates...')
-    prop_data.loc[:, 'x']           = prop_data.loc[:, 'pos'].apply(lambda pos: list(map(lambda p: p.x, pos))) # \
-    print('Calculating y coordinates...')                                                                      #  \
-    prop_data.loc[:, 'y']           = prop_data.loc[:, 'pos'].apply(lambda pos: list(map(lambda p: p.y, pos))) # --> TODO: Condense into single statement
-    print('Calculating z coordinates...')                                                                      #  /
-    prop_data.loc[:, 'z']           = prop_data.loc[:, 'pos'].apply(lambda pos: list(map(lambda p: p.z, pos))) # /
+    prop_data['x']          = prop_data['pos'].apply(lambda pos: list(map(lambda p: p.x, pos))) # \
+    print('Calculating y coordinates...')                                                       #  \
+    prop_data['y']          = prop_data['pos'].apply(lambda pos: list(map(lambda p: p.y, pos))) # --> TODO: Condense into single statement
+    print('Calculating z coordinates...')                                                       #  /
+    prop_data['z']          = prop_data['pos'].apply(lambda pos: list(map(lambda p: p.z, pos))) # /
+
+    # prop_data[['x', 'y', 'z']] = prop_data['pvs'].apply(lambda pvs: list(map(lambda pos: [pos.x, pos.y, pos.z], list(map(lambda pv: pv.getPosition(), pvs)))))
+
     print('Calculating radii...')
-    prop_data.loc[:, 'radius']      = prop_data.loc[:, 'pos'].apply(lambda pos: list(map(lambda p: np.sqrt(p.x**2 + p.y**2 + p.z**2), pos)))
+    prop_data['radius']     = prop_data['pos'].apply(lambda pos: list(map(lambda p: np.sqrt(p.x**2 + p.y**2 + p.z**2), pos)))
 
     print('Calculating groundpoints...')
-    prop_data.loc[:, 'gps']         = prop_data.loc[:, 'pvs'].apply(lambda pvs: list(map(lambda pv: sf.earth(sf.eme2000).transform(pv.position, sf.eme2000, pv.date), pvs)))
+    prop_data['gps']        = prop_data['pvs'].apply(lambda pvs: list(map(lambda pv: sf.earth(sf.eme2000).transform(pv.position, sf.eme2000, pv.date), pvs)))
     print('Calculating latitudes...')
-    prop_data.loc[:, 'latitude']    = prop_data.loc[:, 'gps'].apply(lambda gps: list(map(lambda gp: np.degrees(gp.latitude), gps)))
+    prop_data['latitude']   = prop_data['gps'].apply(lambda gps: list(map(lambda gp: np.degrees(gp.latitude), gps)))
     print('Calculating longitudes...')
-    prop_data.loc[:, 'longitude']   = prop_data.loc[:, 'gps'].apply(lambda gps: list(map(lambda gp: np.degrees(gp.longitude), gps)))
+    prop_data['longitude']  = prop_data['gps'].apply(lambda gps: list(map(lambda gp: np.degrees(gp.longitude), gps)))
 
-    prop_data.drop(['pvs', 'pos', 'gps'], axis=1).to_pickle('Propagation_Data.pkl')
+    prop_data.drop(['pvs', 'pos', 'gps'], axis=1).to_json('Propagation_Data.json')
     return prop_data
 
 def updateMetadata(prop_data):
@@ -66,8 +68,6 @@ def updateMetadata(prop_data):
     with open(metadata_file, 'w') as file:
         json.dump(metadata, file)
 
-
-
 with open((metadata_file := 'Metadata.json'), 'r') as file:
     metadata = json.load(file)
 
@@ -76,5 +76,5 @@ prop_data   = propagateOrbits(sat_data)
 updateMetadata(prop_data)
 
 print('Tracing orbits...')
-pf = PlotFunctions(prop_data_file='Propagation_Data.pkl')
-pf.plotOrbits(prop_metadata_file='Propagation_Metadata.json')
+pf = PlotFunctions(prop_data_file='Propagation_Data.json')
+pf.plotOrbits(metadata_file='Metadata.json')
